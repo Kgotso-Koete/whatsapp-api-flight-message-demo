ef01036a3d91e569ba1c0f4c0277a8f4a334e60f
Kgotso-Koete <kgkoete@gmail.com>
2025-04-19 00:27:33 +0200

    chanfge PDF document link

--- flights.py
+++ flights.py
@@ -6,9 +6,10 @@ This source code is licensed under the license found in the
 LICENSE file in the root directory of this source tree.
 """
 def get_flights():
+  pfd_document_link = "https://github.com/Kgotso-Koete/whatsapp-api-flight-message-demo/blob/master/FlightConfirmation.pdf"
   return [{
     "flight_id": 1,
-    "document": 'https://github.com/fbsamples/whatsapp-api-examples/blob/main/send-messages-flight-app-python/FlightConfirmation.pdf',
+    "document": pfd_document_link,
     "thumbnail": 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Aerial_view_of_Bajra_Sandhi_Monument_Denpasar_Bali_Indonesia.jpg/250px-Aerial_view_of_Bajra_Sandhi_Monument_Denpasar_Bali_Indonesia.jpg',
     "origin": 'Singapore (SIN)',
     "destination": 'Denpasar (DPS)',
@@ -16,7 +17,7 @@ def get_flights():
   },
   {
     "flight_id": 2,
-    "document": 'https://github.com/fbsamples/whatsapp-api-examples/blob/main/send-messages-flight-app-python/FlightConfirmation.pdf',
+    "document": pfd_document_link,
     "thumbnail": 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Parliament_at_Sunset.JPG/275px-Parliament_at_Sunset.JPG',
     "origin": 'New York (JFK)',
     "destination": 'London (LHR)',
@@ -24,7 +25,7 @@ def get_flights():
   },
   {
     "flight_id": 3,
-    "document": 'https://github.com/fbsamples/whatsapp-api-examples/blob/main/send-messages-flight-app-python/FlightConfirmation.pdf',
+    "document": pfd_document_link,
     "thumbnail": 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Sydney_Australia._%2821339175489%29.jpg/250px-Sydney_Australia._%2821339175489%29.jpg',
     "origin": 'Beijing (PEK)',
     "destination": 'Sydney (SYD)',
@@ -32,7 +33,7 @@ def get_flights():
   },
   {
     "flight_id": 4,
-    "document": 'https://github.com/fbsamples/whatsapp-api-examples/blob/main/send-messages-flight-app-python/FlightConfirmation.pdf',
+    "document": pfd_document_link,
     "thumbnail": 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Mouth_of_Miami_River_20100211.jpg/220px-Mouth_of_Miami_River_20100211.jpg',
     "origin": 'São Paulo (GRU)',
     "destination": 'Miami (MIA)',
